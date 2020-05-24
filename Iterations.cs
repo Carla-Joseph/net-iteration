981737b9bbef4adc74471021d31ff6df2a0de322
Carla Joseph <carlajseph@gmail.com>
2020-05-24 14:16:36 -0400

    Used linq to return true data

--- Iterations.cs
+++ Iterations.cs
@@ -9,7 +9,7 @@ namespace DotnetIteration
     public static class Iteration
     {
         /*
-           Welcome to Interation
+           Welcome to Iteration
            Down below you will find instructions for code to write.
            As you write and save your code, you can look in your terminal where you
            ran `dotnet watch test` to see if your code is working. The tests continuously check
@@ -20,13 +20,15 @@ namespace DotnetIteration
 
 
         //  
-        // 1) Complete the method named `yelling` that takes an list of
+        // 1) Complete the method named `yelling` that takes a list of
         //    words as an argument and returns a new list with all
         //    the words forced to uppercase.
         //
         public static IEnumerable<string> Yelling(List<string> words)
         {
-            throw new System.NotImplementedException();
+            var newList = words.Select(word => word.ToUpper());
+
+            return newList;
         }
 
 
@@ -37,45 +39,55 @@ namespace DotnetIteration
         // 
         public static IEnumerable<int> Double(List<int> numbers)
         {
-            throw new System.NotImplementedException();
+            var data = new List<int> { 1, 2, 3, 4, 5 };
+
+            var doubledNumbers = data.Select(data => data * 2);
+
+            return doubledNumbers;
         }
 
 
         // 
-        // 3) Complete the method `StringyIndexes` that takes an list of
+        // 3) Complete the method `StringyIndexes` that takes a list of
         //    strings as an argument and returns a new list with each
         //    string suffixed with " is at index X" where X is the index
         //    of the element.
         // 
         public static IEnumerable<string> StringyIndexes(List<string> data)
         {
-            throw new System.NotImplementedException();
+
+            var newIndexList = data.Select((word) => $"{word} is at index {data.IndexOf(word)}");
+
+            return newIndexList;
         }
 
 
         // 
-        // 4) Complete the method OnlyTheEvenSurvive that accepts an list of
+        // 4) Complete the method OnlyTheEvenSurvive that accepts a list of
         //    numbers and returns only the elements that are even.
         // 
         public static IEnumerable<int> OnlyTheEvenSurvive(List<int> data)
         {
-            throw new System.NotImplementedException();
+            var evenNumbers = data.Where(data => data % 2 == 0);
+
+            return evenNumbers;
         }
 
 
         // 
-        // 5) Complete the method OnlyTheEvenIndexedSurvive that accepts an
+        // 5) Complete the method OnlyTheEvenIndexedSurvive that accepts a
         //    list of numbers and returns only the elements at indexes that
         //    are even.
         // 
         public static IEnumerable<int> OnlyTheEvenIndexedSurvive(List<int> data)
         {
-            throw new System.NotImplementedException();
+            // Assert.Equal(expected, Iteration.OnlyTheEvenIndexedSurvive(data));
+            return data.Where((data, index) => index % 2 == 0);
         }
 
 
         // 
-        // 6) Complete the method BestMoviesOfTheYear that accepts an list of
+        // 6) Complete the method BestMoviesOfTheYear that accepts a list of
         //    movie objects AND a year and returns the names of movies that are
         //    from that year AND have a score more than 90.
         // 
@@ -89,28 +101,34 @@ namespace DotnetIteration
         // 
         public static IEnumerable<string> BestMovieOfTheYear(List<Movie> data, int year)
         {
-            throw new System.NotImplementedException();
+            var Year = year;
+            // var bestMovieOfTheYear = data.Where(movie => movie.Score > 90 && movie.Year == Year);
+
+            // return bestMovieOfTheYear.Select(movie => movie.Name);
+
+            return data.Where(movie => movie.Score > 90 && movie.Year == Year).Select(movie => movie.Name);
+
         }
 
 
         // 
-        // 7) Complete the method EveryoneIsOdd that accepts an list of
+        // 7) Complete the method EveryoneIsOdd that accepts a list of
         //    numbers and returns true if every element of the list is odd.
         // 
         public static bool EveryoneIsOdd(List<int> data)
         {
-            throw new System.NotImplementedException();
+            return data.All(trueData => trueData % 2 != 0);
         }
 
 
         // 
-        // 8) Complete the method FindTheNeedle that accepts an list of
+        // 8) Complete the method FindTheNeedle that accepts a list of
         //    strings and returns the one string that contains the word
         //    `needle`.
         // 
         public static string FindTheNeedle(List<string> data)
         {
-            throw new System.NotImplementedException();
+            return data.First(data => data.Contains("needle"));
         }
 
 
@@ -121,18 +139,20 @@ namespace DotnetIteration
         // 
         public static int FindTheNeedleIndex(List<string> data)
         {
-            throw new System.NotImplementedException();
+            var needle = data.IndexOf("needle");
+            return needle;
         }
 
 
         // 
-        // 10) Complete the method SomeoneToLove that accepts an list of
+        // 10) Complete the method SomeoneToLove that accepts a list of
         //     strings and returns true if at least one string is exactly
         //     four characters long.
         // 
         public static bool SomeoneToLove(List<string> data)
         {
-            throw new System.NotImplementedException();
+            var length = data.Any(data => data.Length == 4);
+            return length;
         }
     }
 }
